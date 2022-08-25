258a7a61c3c0e7500de67ad48f2799b411610740
uheaji <yoolbk@naver.com>
2022-08-25 16:09:23 +0900

    #3.2 Typing the Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
-interface ContainerProps {
-    bgColor: string;
-}
+// interface ContainerProps {
+//     bgColor: string;
+// }
 
-const Container = styled.div<ContainerProps>`
-    width: 200px;
-    height: 200px;
-    background-color: ${props => props.bgColor};
-    border-radius: 100px;
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
 `;
 
 interface CircleProps {
