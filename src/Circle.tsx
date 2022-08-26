80a95c8055b91b3d770c315bd6be0c15398fbbb3
uheaji <yoolbk@naver.com>
2022-08-26 09:21:53 +0900

    #3.3 Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
-// interface ContainerProps {
-//     bgColor: string;
-// }
+interface ContainerProps {
+    bgColor: string;
+    borderColor: string;
+}
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string; // optional props('?'를 이용)
+    text?: string;
 }
 
-function Circle({bgColor}: CircleProps) {
-    return <Container bgColor={bgColor} />
+function Circle({bgColor, borderColor, text = 'default text'}: CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
+    </Container>; // borderColor가 undefined 일때 (??)
 }
 
 export default Circle;
