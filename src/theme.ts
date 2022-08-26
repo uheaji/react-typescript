9018f6f168f68f23bfdd18e45e2e18238d2fb322
uheaji <yoolbk@naver.com>
2022-08-26 12:22:06 +0900

    #4.1 Styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-    bgColor: "white",
-    textColor: "balck",
-    btnColor: "tomato"
-}
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+};
