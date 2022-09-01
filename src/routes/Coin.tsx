d844b5bdde666d63ffa775a7d2e446980756accc
uheaji <yoolbk@naver.com>
2022-09-01 17:17:43 +0900

    #4.7 Nested Routes part1

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,8 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Route, Switch, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
-
-interface RouteParams {
-    coinId: string;
-}
-
-interface RouteState {
-    name: string;
-}
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,11 +11,10 @@ const Container = styled.div`
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 14vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  justify-content: center;
 `;
 
 const Title = styled.h1`
@@ -29,12 +22,43 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
-
 const Loader = styled.span`
   text-align: center;
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 15px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+interface RouteParams {
+  coinId: string;
+}
+
+interface RouteState {
+  name: string;
+}
 
 interface InfoData {
   id: string;
@@ -57,7 +81,6 @@ interface InfoData {
   last_data_at: string;
 }
 
-
 interface PriceData {
   id: string;
   name: string;
@@ -73,7 +96,7 @@ interface PriceData {
     USD: {
       ath_date: string;
       ath_price: number;
-      market_cap: number
+      market_cap: number;
       market_cap_change_24h: number;
       percent_change_1h: number;
       percent_change_1y: number;
@@ -93,35 +116,75 @@ interface PriceData {
 }
 
 function Coin() {
-    const [loading, setLoading] = useState(true);
-    // useParams(): url에서 관심있어하는 정보를 잡아낼 수 있게 해준다.
-    const { coinId } = useParams<RouteParams>();
-    const { state } = useLocation<RouteState>();
-    const [info, setInfo] = useState<InfoData>();
-    const [priceInfo, setPriceInfo] = useState < PriceData>();
-    useEffect(() => {
-        (async () => {
-            const infoData = await (
-                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-            ).json();
-            console.log(infoData)
-            const priceData = await(
-                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-            ).json();
-            console.log(priceData);
-            setInfo(infoData);
-            setPriceInfo(priceData);
-            setLoading(false);
-        })();
-    }, []);
-    return (
-        <Container>
-            <Header>
-                <Title>{state?.name || "Loading.."}</Title>
-            </Header>
-            {loading ? <Loader>"Loading..."</Loader> : null}
-        </Container>
-    );
+  const [loading, setLoading] = useState(true);
+  // useParams(): url에서 관심있어하는 정보를 잡아낼 수 있게 해준다.
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  useEffect(() => {
+    (async () => {
+      // 코인정보 가져오기
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      // 코인가격정보 가져오기
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, [coinId]);
+  return (
+    <Container>
+      <Header>
+        <Title>
+          {state?.name ? state?.name : loading ? "Loading.." : info?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>"Loading..."</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source::</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total supply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default Coin;
